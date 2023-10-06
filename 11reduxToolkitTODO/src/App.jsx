2ee9d415b905eba5ffdc2c71adf97fbcb95d856f
Rishu <kumarrishu571@gmail.com>
2023-10-06 22:07:34 +0530

    Learn Redux toolkit

---
+++ 11reduxToolkitTODO/src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+
+    </>
+  )
+}
+
+export default App
---
+++ 11reduxToolkitTODO/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+import {createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = {
+    todos : [{id : 1, text : "Hello World"}]
+}
+
+
+export const todoSlice = createSlice({
+    name: 'todo', 
+    initialState,
+    reducers: {
+        addTodo : (state, action) => {
+            const todo = {
+                id : nanoid(), 
+                text : action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
