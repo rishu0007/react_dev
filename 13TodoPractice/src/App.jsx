4530f446c659a7c1242ab3095a41c44fbcf823b9
Rishu <kumarrishu571@gmail.com>
2023-10-09 23:16:22 +0530

    Practice todo

---
+++ 13TodoPractice/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import Todo from './Todo'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
